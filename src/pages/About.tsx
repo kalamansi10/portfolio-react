91b04d8a875b95295b7492b32f56a9029a6656ec
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2024-04-08 22:03:57 +0800

    Initial about page layout

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,7 +2,34 @@ const About: React.FC = () => {
   return (
     <div>
       <h2>About</h2>
-      <p></p>
+      <p>
+        Hi, I'm Jaymar. I'm a self-taught, self-driven, full stack web
+        developer. I have been doing web development for over a year now, and I
+        am eager to contribute my skills to help industries build products that
+        can provide people with meaningful experiences.
+      </p>
+      <h2>Skills</h2>
+      <h3>Front End</h3>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Javascript</li>
+        <li>Typescript</li>
+        <li>React.js</li>
+      </ul>
+      <h3>Back End</h3>
+      <ul>
+        <li>Node.js</li>
+        <li>Ruby on Rails</li>
+        <li>MongoDB</li>
+        <li>PostgreSQL</li>
+      </ul>
+      <h3>Others</h3>
+      <ul>
+        <li>GitHub</li>
+        <li>Vite</li>
+        <li>Adobe Tools</li>
+      </ul>
     </div>
   );
 };
