173abe1d66333e8d524914ab32ad6f8ae8468dd7
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2024-04-08 22:04:33 +0800

    Initial contact page layout

--- src/pages/ReachOut.tsx
+++ src/pages/ReachOut.tsx
@@ -1,7 +1,13 @@
 const ReachOut: React.FC = () => {
   return (
     <div>
-      <h2>ReachOut</h2>
+      <h2>Reach Out To Me</h2>
+      <h3>Email</h3>
+      <a href="mailto: jjum1337@outlook.com">jjum1337@outlook.com</a>
+      <h3>GitHub</h3>
+      <a href="https://github.com/kalamansi10">GitHub</a>
+      <h3>LinkedIn</h3>
+      <a href="https://www.linkedin.com/in/jjumadiao">LinkedIn</a>
     </div>
   );
 };
