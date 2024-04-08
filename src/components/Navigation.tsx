2b8d8442708233d58a5297352ecf23d26c00e381
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2024-04-08 22:02:56 +0800

    Added hamburger menu button for small screens

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,13 +3,23 @@ import { Link } from "react-router-dom";
 const Navigation: React.FC = () => {
   return (
     <nav>
-      <Link to="/"><h1>Jaymar Jumadiao</h1></Link>
-      <ul>
-        <li>
-          <Link to="/projects">Projects</Link>
-        </li>
+      <Link to="/">
+        <h1 className="nav-header">Jaymar Jumadiao</h1>
+      </Link>
+      <input type="checkbox" id="nav-toggle" />
+      <div className="burger-wrapper">
+        <label htmlFor="nav-toggle">
+          <div className="burger">
+            <i>Menu</i>
+          </div>
+        </label>
+      </div>
+      <ul className="nav-links">
         <li>
           <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projects</Link>
         </li>
         <li>
           <Link to="/reach-out">Reach Out</Link>
