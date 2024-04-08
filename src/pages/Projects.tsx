fe51bd9b061661c735349301b0694aad310a453a
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2024-04-08 22:04:12 +0800

    Initial projects page layout

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,7 +1,26 @@
+import projects from "../../public/projects.json";
+import ProjectWrapper from "../components/ProjectWrapper";
+
+interface Project {
+  name: string;
+  description: string;
+  technologies: string[];
+  source_code_link: string;
+  live_preview_link: string;
+  preview_images: string[];
+}
+
 const Projects: React.FC = () => {
+  const renderProjects = () => {
+    return projects.map((project: Project, index: number) => {
+      return <ProjectWrapper key={index} project={project} />;
+    });
+  };
+
   return (
     <div>
       <h2>Projects</h2>
+      {renderProjects()}
     </div>
   );
 };
