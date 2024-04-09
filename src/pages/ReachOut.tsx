1fc5d086c72cd7bcaead61728bfc4d2c6e3c4fce
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2024-04-09 16:02:39 +0800

    Added contact details

--- src/pages/ReachOut.tsx
+++ src/pages/ReachOut.tsx
@@ -1,13 +1,31 @@
 const ReachOut: React.FC = () => {
   return (
-    <div>
+    <div className="contact-page">
       <h2>Reach Out To Me</h2>
-      <h3>Email</h3>
-      <a href="mailto: jjum1337@outlook.com">jjum1337@outlook.com</a>
-      <h3>GitHub</h3>
-      <a href="https://github.com/kalamansi10">GitHub</a>
-      <h3>LinkedIn</h3>
-      <a href="https://www.linkedin.com/in/jjumadiao">LinkedIn</a>
+      <a className="contact-info" href="mailto: jjum1337@outlook.com" >
+        <img src="./src/assets/envelope-icon.svg" alt="Email" />
+        <div>
+          <h3>Email</h3>
+          <br />
+          <p>jjum1337@outlook.com</p>
+        </div>
+      </a>
+      <a className="contact-info" href="https://github.com/kalamansi10">
+        <img src="./src/assets/github-icon.svg" alt="GitHub" />
+        <div>
+          <h3>GitHub</h3>
+          <br />
+          <p>GitHub</p>
+        </div>
+      </a>
+      <a className="contact-info" href="https://www.linkedin.com/in/jjumadiao">
+        <img src="./src/assets/linkedin-square-icon.svg" alt="LinkedIn" />
+        <div>
+          <h3>LinkedIn</h3>
+          <br />
+          <p>LinkedIn</p>
+        </div>
+      </a>
     </div>
   );
 };
