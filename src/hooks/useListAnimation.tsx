ddfe0d4477acd46110ece6280a72d745f48ad69a
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2024-04-12 09:23:37 +0800

    Renamed listItem dom vaiable

--- src/hooks/useListAnimation.tsx
+++ src/hooks/useListAnimation.tsx
@@ -2,14 +2,14 @@ import { useEffect } from "react";
 
 const useListAnimation = () => {
   useEffect(() => {
-    const skillList: NodeListOf<HTMLElement> = document.querySelectorAll(".anim-list-item");
+    const listItem: NodeListOf<HTMLElement> = document.querySelectorAll(".anim-list-item");
   
-    skillList.forEach((item: HTMLElement, index: number) => {
+    listItem.forEach((item: HTMLElement, index: number) => {
       setTimeout(() => item.classList.add("up-anim"), 100 * (index + 1));
     });    
 
     return () => {
-      skillList.forEach((item: HTMLElement) => {
+      listItem.forEach((item: HTMLElement) => {
         item.classList.remove("up-anim");
       });
     };
