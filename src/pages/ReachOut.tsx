48774f3699c60faf9949e652bfb5a340424e7686
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2024-04-12 09:24:20 +0800

    Added delay for contact info hover animation

--- src/pages/ReachOut.tsx
+++ src/pages/ReachOut.tsx
@@ -1,12 +1,28 @@
+import { useEffect } from "react";
 import useListAnimation from "../hooks/useListAnimation";
 
 const ReachOut: React.FC = () => {
+
+  useEffect(() => {
+    const contactInfo: NodeListOf<HTMLElement> = document.querySelectorAll(".contact-info");
+
+    contactInfo.forEach((item: HTMLElement, index: number) => {
+      setTimeout(() => item.classList.add("steam-anim"), 1000 * (index + 1));
+    });    
+
+    return () => {
+      contactInfo.forEach((item: HTMLElement) => {
+        item.classList.remove("up-anim");
+      });
+    };
+  }, [])
+
   useListAnimation()
 
   return (
     <div className="contact-page">
       <h2>Reach Out To Me</h2>
-      <a className="contact-info steam-anim anim-list-item" href="mailto: jjum1337@outlook.com" >
+      <a className="contact-info anim-list-item" href="mailto: jjum1337@outlook.com" >
         <img src="./src/assets/envelope-icon.svg" alt="Email" />
         <div>
           <h3>Email</h3>
@@ -14,7 +30,7 @@ const ReachOut: React.FC = () => {
           <p>jjum1337@outlook.com</p>
         </div>
       </a>
-      <a className="contact-info steam-anim anim-list-item" href="https://github.com/kalamansi10">
+      <a className="contact-info anim-list-item" href="https://github.com/kalamansi10">
         <img src="./src/assets/github-icon.svg" alt="GitHub" />
         <div>
           <h3>GitHub</h3>
@@ -22,7 +38,7 @@ const ReachOut: React.FC = () => {
           <p>GitHub</p>
         </div>
       </a>
-      <a className="contact-info steam-anim anim-list-item" href="https://www.linkedin.com/in/jjumadiao">
+      <a className="contact-info anim-list-item" href="https://www.linkedin.com/in/jjumadiao">
         <img src="./src/assets/linkedin-square-icon.svg" alt="LinkedIn" />
         <div>
           <h3>LinkedIn</h3>
