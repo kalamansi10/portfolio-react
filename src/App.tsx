275fa85510d252c3631d06f78b7a022adf13a945
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2024-05-06 23:26:06 +0800

    Implemented code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Hero from "./components/Hero";
 import Navigation from "./components/Navigation";
-import About from "./pages/About";
-import Projects from "./pages/Projects";
-import ReachOut from "./pages/ReachOut";
 import "./App.css";
+
+const Hero = lazy(() => import("./components/Hero"));
+const About = lazy(() => import("./pages/About"));
+const Projects = lazy(() => import("./pages/Projects"));
+const ReachOut = lazy(() => import("./pages/ReachOut"));
 
 const App: React.FC = () => {
   return (
@@ -13,10 +15,38 @@ const App: React.FC = () => {
         <Navigation />
         <div className="pages">
           <Routes>
-            <Route index element={<Hero />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/reach-out" element={<ReachOut />} />
+            <Route
+              index
+              element={
+                <Suspense>
+                  <Hero />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/about"
+              element={
+                <Suspense>
+                  <About />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/projects"
+              element={
+                <Suspense>
+                  <Projects />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/reach-out"
+              element={
+                <Suspense>
+                  <ReachOut />
+                </Suspense>
+              }
+            />
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </div>
